32e69a9d05bb84b0d55be5102480b1c354dac218
Sadel <sadel@Sadels-MacBook-Pro.local>
2022-04-25 02:59:12 +0300

    .

--- lib/core/services/mock_services.dart
+++ lib/core/services/mock_services.dart
@@ -5,24 +5,46 @@ import 'package:beltareeq/core/models/mock_location_model.dart';
 
 class MockService {
   List<LocationModel> savedmockLoction = [
-    LocationModel(isFavrite: false, locationName: "Work", locationSubTitle: "Abdali"),
-    LocationModel(isFavrite: false, locationName: "University", locationSubTitle: "Queen Rania Street"),
-    LocationModel(isFavrite: true, locationName: "Home", locationSubTitle: "Khelda"),
+    LocationModel(
+        isFavrite: false, locationName: "Work", locationSubTitle: "Abdali"),
+    LocationModel(
+        isFavrite: false,
+        locationName: "University",
+        locationSubTitle: "Queen Rania Street"),
+    LocationModel(
+        isFavrite: true, locationName: "Home", locationSubTitle: "Khelda"),
   ];
   List<LocationModel> recentmockLoction = [
-    LocationModel(isFavrite: true, locationName: "Beyond Limits", locationSubTitle: "Abdali"),
-    LocationModel(isFavrite: false, locationName: "PSUT", locationSubTitle: "Queen Rania Street"),
-    LocationModel(isFavrite: true, locationName: "Home", locationSubTitle: "Seventh Circle")
+    LocationModel(
+        isFavrite: true,
+        locationName: "Beyond Limits",
+        locationSubTitle: "Abdali"),
+    LocationModel(
+        isFavrite: false,
+        locationName: "PSUT",
+        locationSubTitle: "Queen Rania Street"),
+    LocationModel(
+        isFavrite: true,
+        locationName: "Home",
+        locationSubTitle: "Khelda")
   ];
   List<RideOptionsMockModel> rideOptionModel = [
     RideOptionsMockModel(
-        image: "assets/images/uper.png", isBestPrice: true, price: "2jd", time: "2 min", color: ColorConstants.color_0xf0000000),
+        image: "assets/images/uper.png",
+        isBestPrice: true,
+        price: "3.5jd",
+        time: "2 min",
+        color: ColorConstants.color_0xf0000000),
     RideOptionsMockModel(
-        image: "assets/images/jeeny.png", isBestPrice: false, price: "4jd", time: "5 min", color: ColorConstants.color_0xffb0bfb),
+        image: "assets/images/jeeny.png",
+        isBestPrice: false,
+        price: "3jd",
+        time: "15 min",
+        color: ColorConstants.color_0xffb0bfb),
     RideOptionsMockModel(
         image: "assets/images/careem.png",
         isBestPrice: false,
-        price: "3jd",
+        price: "4jd",
         time: "10 min",
         color: ColorConstants.color_0xf3cb44c)
   ];
